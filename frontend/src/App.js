4673d3464dead2180fda1b7357a19812820dfcc9
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-02 21:06:57 +0530

    added header to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import Header from './components/Header'
+import Container from 'react-bootstrap'
 const App = () => {
   return (
-    <div>
-      Debaya
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+
+    </>
   )
 }
 
