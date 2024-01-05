97c21f85b9d72dcd995a292b4588163d86e33e3e
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-05 11:42:27 +0530

    creating routes  for homescreen and outlet on appjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen';
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Outlet />
         </Container>
       </main>
       <Footer />
