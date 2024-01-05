93984ce47b61d58143ab82d025b5c10a93226061
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-05 19:03:48 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import products from './data/products.js';
-
+import connectDB from './config/db.js';
 import dotenv from 'dotenv'
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+connectDB(); //connect to mongodb
 const app = express();
 
 app.get('/', (req, res) => {
