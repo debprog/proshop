f0fe188a2845a4dc24c13b0266c21f82802b2f21
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-06 14:52:46 +0530

    seeder file help to load data on the database or to delete from database

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import colors from "colors";
 
-import users from "./data/users";
-import products from "./data/products";
+import users from './data/users.js';
+import products from './data/products.js';
 
-import User from './models/userModel';
-import Product from "./models/productModel";
-import Order from "./models/orderModel";
+import User from './models/userModel.js';
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
 
-import connectDB from './config/db';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
@@ -61,7 +61,7 @@ if (process.argv[2] === '-d') {
     destroyData();
 }
 else {
-    importData()
+    importData();
 }
 
 
