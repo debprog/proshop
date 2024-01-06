22578e1b7f4bcd20496ce58138e46a5cec140713
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-06 12:42:47 +0530

    installed bcryptjs creates dummy users

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmnin: true,
+    },
+
+    {
+        name: 'Debayan Pal',
+        email: 'debayan@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmnin: false,
+    },
+
+    {
+        name: 'Arnab Ghosh',
+        email: 'arnab@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmnin: false,
+    }
+];
+
+
+export default users;
