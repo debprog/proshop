03463df0c6d2f1c4038fc62416400aab80391e40
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-06 19:38:08 +0530

    added errorhandler middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -15,6 +16,9 @@ app.get('/', (req, res) => {
 )
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => {
