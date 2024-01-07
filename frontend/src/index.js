ba07748a278cbf018f897fadca9e50b61c694db4
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-07 12:59:40 +0530

    installed redux created constants for URLs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App';
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -24,7 +26,10 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
