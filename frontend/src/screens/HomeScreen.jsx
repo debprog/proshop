4ac5fdee45be32ad8b0c8b06aaab3bd055ba8cdd
Debayan Pal <paldebayan19982016@gmail.com>
2024-01-10 23:33:02 +0530

    Created productapi slice changing base url as ' ' because proxy is being used

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import { Row,Col } from 'react-bootstrap'
-import { useEffect,useState } from 'react';
+import { useGetProductsQuery } from '../slices/productsApiSlices';
 import Product from '../components/Product';
-import axios from 'axios';
+
 const HomeScreen = () => {
 
-  const [products,setProducts]=useState([]);
-
-  useEffect(() => {
-    const fetchProducts= async() => {
-      const {data}= await axios.get('/api/products');
-      setProducts(data);
-    };
-
-    fetchProducts();
-    
-  },[]);
+  const {data:products,isLoading,error }=useGetProductsQuery();
+  
   return (
     <>
-      <h1>Latest Product</h1>
+
+    {isLoading?(
+      <h2>Loading.....</h2>
+    ):error ?(
+      <div>{error?.data?.message || error.error}</div>
+    ):
+    
+    (<><h1>Latest Product</h1>
       <Row>
         {products.map((product)=>(
          <Col key={product._id}sm={12} md={6} lg={4} xl={3}>
            <Product product={product} />
          </Col>
         ))}
-      </Row>
+      </Row></>)}
+      
     </>
   )
 }
 
-export default HomeScreen
+export default HomeScreen; 
